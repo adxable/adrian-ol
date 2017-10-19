25851e98672f8b0666d050aba9e3d00ac2f5bbdd
adxable <adrian.oleskiewicz@gmail.com>
2017-10-19 05:13:38 +0200

    add files

--- js/pre/pageScroll.js
+++ js/pre/pageScroll.js
@@ -1,20 +1,20 @@
 
-jQuery(document).ready(function(){
-  jQuery('a[href*=\\#]:not([href=\\#])').click(function() {
+$(document).ready(function(){
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-      var target = jQuery(this.hash);
-      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        jQuery('html, body').animate({
+        $('html, body').animate({
           scrollTop: target.offset().top}, 500);
           return false;
       }
     }
   });
 });
-jQuery(window).on('load', function(){
+$(window).on('load', function(){
   function goToByScroll(id){
-    jQuery("html, body").animate({scrollTop: jQuery("#"+id).offset().top}, 500);
+    $("html, body").animate({scrollTop: $("#"+id).offset().top}, 500);
   }
   if(window.location.hash != '') {
     goToByScroll(window.location.hash.substr(1));
